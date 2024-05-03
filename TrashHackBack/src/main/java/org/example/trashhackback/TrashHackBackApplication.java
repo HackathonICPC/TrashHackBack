ea0b1d3d42e9b58245c1b06be23d7caa5c56b95c
sour-soup <kiziyepi8@gmail.com>
2024-05-03 12:51:29 +0400

    First commit

---
+++ TrashHackBack/src/main/java/org/example/trashhackback/TrashHackBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.trashhackback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashHackBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrashHackBackApplication.class, args);
+    }
+
+}
---
+++ TrashHackBack/src/test/java/org/example/trashhackback/TrashHackBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.trashhackback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrashHackBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
